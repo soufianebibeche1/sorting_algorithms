7981614c015b54d0148c3ac424a13887f299bdf9
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-17 01:14:22 +0000

    tes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,30 +1,38 @@
+/* 0-bubble_sort.c */
+
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of int in ascending order
+ * bubble_sort - Sorts an array in ascending order using Bubble sort
  * @array: The array to be sorted
- * @size: Number of elements in the array
- *
- * Return: void
+ * @size: The size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, temp;
+	size_t i, j;
+	int temp;
+	int swapped;
 
-	if (size < 2)
-		return;
-
-	for (i = 0; i <= size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j <= size - 2; j++)
+		swapped = 0;
+
+		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				swapped = 1;
+
+				/* Print array after each swap */
 				print_array(array, size);
 			}
 		}
+
+		/* If no two elements were swapped in inner loop, array is sorted */
+		if (!swapped)
+			break;
 	}
 }
