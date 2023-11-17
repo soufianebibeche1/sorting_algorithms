451e45f91992d2007f005d69489a3010de22e34d
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-17 02:06:09 +0000

    Implement Bubble Sort and provide Big O notations

--- sort.h
+++ sort.h
@@ -1,9 +1,9 @@
 #ifndef __SORT__H__
 #define __SORT__H__
+
 #include <stdio.h>
 #include <stdlib.h>
 
-/*---- Provided struct ----*/
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,28 +18,15 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/*---- Provided functions ----*/
-void print_list(const listint_t *list);
+/* provided functions */
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+listint_t *create_listint(const int *array, size_t size);
 
-/*---- Our required functions ----*/
+/* TASK Functions */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-/*---- Our helper functions ----*/
-int checklist(listint_t **list, listint_t **current);
-void checkend(listint_t **current, listint_t **tmp, listint_t *list);
-int checkbeg(listint_t **current, listint_t **tmp,
-	     listint_t **hold, listint_t **list);
-int checkjuice(listint_t **current, listint_t **tmp,
-	       listint_t *list);
-int partition(int *array, size_t start, size_t end, size_t size);
-void realqs(int *array, int start, int end, size_t size);
-void counting_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void split_array(int *comparray, size_t left, size_t right, int *array);
-void merge_arrays(int *array, size_t left, size_t middle, size_t right,
-int *comparray);
-#endif
+#endif /* sort.h */
