db6675a720d7c957b600dea8f26c225dab1cc959
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-17 01:01:20 +0000

    testem

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,21 +11,18 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, temp;
 
-	if (array == NULL || size < 2)
+	if (size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i <= size - 1; i++)
 	{
-		for (j = 0; j < size - 2; j++)
+		for (j = 0; j <= size - 2; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				/* Swap the elements */
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-
-				/* Print the array after each swap */
 				print_array(array, size);
 			}
 		}
