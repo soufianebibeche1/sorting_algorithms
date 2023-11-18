26b6784ad1b824054d67def63e350b617fd729de
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-18 00:50:32 +0000

    test

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,4 @@
 /* 0-bubble_sort.c */
-
 #include "sort.h"
 
 /**
@@ -13,7 +12,7 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, temp;
 
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i <= size - 1; i++)
